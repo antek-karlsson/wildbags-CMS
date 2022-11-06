3c74e85e78af7cb270f14ba90c0962de4c20c023
Antek <antek@antonis-macbook-pro.home>
2022-11-06 20:31:43 +0100

    upload local repo to GH

---
+++ src/App.tsx
@@ -0,0 +1,152 @@
+import React, { useCallback } from 'react';
+
+import { User as FirebaseUser } from 'firebase/auth';
+import {
+  Authenticator,
+  buildCollection,
+  buildProperty,
+  EntityReference,
+  FirebaseCMSApp
+} from '@camberi/firecms';
+
+import 'typeface-rubik';
+import '@fontsource/ibm-plex-mono';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+type Product = {
+  name: string;
+  price: number;
+  status: string;
+  images: string[];
+  description: string;
+  features: string[];
+  sizes: string[];
+  categories: string[];
+};
+
+const productsCollection = buildCollection<Product>({
+  name: 'Products',
+  singularName: 'Product',
+  path: 'products',
+  permissions: ({ authController }) => ({
+    edit: true,
+    create: true,
+    // we have created the roles object in the navigation builder
+    delete: false
+  }),
+  properties: {
+    name: {
+      name: 'Nazwa',
+      validation: { required: true },
+      dataType: 'string'
+    },
+    price: {
+      name: 'Cena',
+      validation: {
+        required: true,
+        requiredMessage: 'Ustaw cenę produktu',
+        min: 0,
+        max: 10000
+      },
+      description: '0 - 10 000',
+      dataType: 'number'
+    },
+    status: {
+      name: 'Status',
+      validation: { required: true },
+      dataType: 'string',
+      description: 'Określ czy produkt ma być widoczny na stronie',
+      enumValues: {
+        private: 'Widoczny',
+        public: 'Ukryty'
+      }
+    },
+    images: buildProperty({
+      dataType: 'array',
+      name: 'Zdjęcia',
+      of: {
+        dataType: 'string',
+        storage: {
+          storagePath: 'images',
+          acceptedFiles: ['image/*'],
+          metadata: {
+            cacheControl: 'max-age=1000000'
+          }
+        }
+      },
+      description: 'Dodaj zdjęcia produktu w pożądanej kolejności'
+    }),
+    description: {
+      name: 'Opis',
+      description: 'Dodaj opis produktu',
+      longDescription:
+        'Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.',
+      dataType: 'string',
+      columnWidth: 300
+    },
+    features: buildProperty({
+      dataType: 'array',
+      name: 'Właściwości',
+      of: {
+        dataType: 'string'
+      },
+      description: 'Dodaj właściwości produktu (lista właściwości)'
+    }),
+    sizes: buildProperty({
+      dataType: 'array',
+      name: 'Wymiary',
+      of: {
+        dataType: 'string'
+      },
+      description: 'Dodaj wymiary produktu (lista wymiarów z opisem)'
+    }),
+    categories: {
+      name: 'Kategorie',
+      validation: { required: true },
+      dataType: 'array',
+      of: {
+        dataType: 'string',
+        enumValues: {
+          kosmetyczki: 'Kosmetyczki',
+          torby: 'Torby'
+        }
+      }
+    }
+  }
+});
+
+export default function App() {
+  const myAuthenticator: Authenticator<FirebaseUser> = useCallback(
+    async ({ user, authController }) => {
+      if (user?.email?.includes('flanders')) {
+        throw Error('Stupid Flanders!');
+      }
+
+      console.log('Allowing access to', user?.email);
+      // This is an example of retrieving async data related to the user
+      // and storing it in the user extra field.
+      const sampleUserRoles = await Promise.resolve(['admin']);
+      authController.setExtra(sampleUserRoles);
+
+      return true;
+    },
+    []
+  );
+
+  return (
+    <FirebaseCMSApp
+      name={'Wildbags'}
+      authentication={myAuthenticator}
+      collections={[productsCollection]}
+      firebaseConfig={firebaseConfig}
+    />
+  );
+}
